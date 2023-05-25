6516eb266987ff1e57fcfa0932c9abba0944937e
Leonardo Jorge Monge <leo.monge1986@gmail.com>
2023-05-25 20:37:50 -0300

    Node y update de carpetas

--- Tienda Sound/app.js
+++ Tienda Sound/app.js
@@ -1,15 +1,18 @@
+//npm init
+//npm install express
+
 const express = require("express");
 const path = require("path");
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.get('/', (req , res) => {
-    res.sendFile (path.resolve(__dirname, './views/home.html'))
+app.get('/', (req, res) =>
+{
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
 
-app.listen(3000, () => {
+app.listen(3000, () =>
+{
     console.log("servidor corriendo")
 });
-
-
