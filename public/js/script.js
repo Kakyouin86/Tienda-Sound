29073308277238eaaf55d5fcba0d1be76e6bfb49
Juan José Barrea <barreajuanjose@gmail.com>
2023-09-09 19:12:48 -0300

    Validación Register en proceso

--- public/js/script.js
+++ public/js/script.js
@@ -4,23 +4,28 @@ window.addEventListener('load', function() {
     let formulario = document.querySelector('form.register-form');
 
     formulario.addEventListener('submit', function(e){ 
-        
         let errores = []
 
+        // Campo NOMBRE
         let camponombre = document.querySelector('#nombreCompleto');
+        let ulErrores = document.querySelector('div.text-danger' );
 
         if(camponombre.value == "") {
             errores.push("Este campo debe estar completo")
+        } else {
+            ulErrores.innerHTML = ''; // Clear the error message if there are no errors
+        }
+        
+        if (camponombre.length < 2) {
+            errores.push("El nombre completo debe contener al menos un nombre y un apellido separados por un espacio");
+        } else {
+            ulErrores.innerHTML = ''; // Clear the error message if there are no errors
         }
 
         if (errores.length > 0) {
-
             e.preventDefault();
-
-            let ulErrores = document.querySelector('div.text-danger' );
-
             for (let i = 0; i<errores.length; i++ ) {
-                ulErrores.innerHTML = "<p>"+ errores[i] +"</p>"
+                ulErrores.innerHTML += "<p>"+ errores[i] +"</p>"
             }
         }
 
