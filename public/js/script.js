cd8d92056d826f9aab761530a60115b5514225b2
Juan José Barrea <barreajuanjose@gmail.com>
2023-09-09 19:43:44 -0300

    Validaciones - Register - en proceso

--- public/js/script.js
+++ public/js/script.js
@@ -1,57 +1,87 @@
 window.addEventListener('load', function() {
-    
     // Campo NOMBRE COMPLETO
     let formulario = document.querySelector('form.register-form');
 
-    formulario.addEventListener('submit', function(e){ 
-        
-        let errores = []
+    formulario.addEventListener('submit', function(e) {
+        let arrayerroresNombre = [];
+        let arrayerroresMail = [];
+        let arrayerroresPassword = [];
 
+
+
+        // Variables IMPUT del Form
         let camponombre = document.querySelector('#nombreCompleto');
+        let erroresNombre = document.querySelector('div.text-danger.nombre');
 
-        if(camponombre.value == "") {
-            errores.push("Este campo debe estar completo")
-        }
+        let campomail = document.querySelector('#email');
+        let erroresMail = document.querySelector('div.text-danger.mail');
 
-        if (errores.length > 0) {
+        let campoPassword = document.querySelector('#password');
+        let erroresPassword = document.querySelector('div.text-danger.pass');
 
-            e.preventDefault();
 
-            let ulErrores = document.querySelector('div.text-danger' );
 
-            for (let i = 0; i<errores.length; i++ ) {
-                ulErrores.innerHTML = "<p>"+ errores[i] +"</p>"
+        // NOMBRE
+        if (camponombre.value.trim() === "") {
+            errores.push("Este campo debe estar completo");
+        } else {
+            // Check if it contains both a first name and a last name separated by a space
+            let nameParts = camponombre.value.trim().split(' ');
+            if (nameParts.length < 2) {
+                errores.push("El nombre completo debe contener al menos un nombre y un apellido separados por un espacio");
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        if(nombreCompleto.value.length < 1){
-            alert("hola1111111")
-            errores.name = 'Este campo debe estar completo'
-        }
-
-
-
-        if(Object.keys(errores).length >= 1){
-
-            erNombre.innerHTML = (errores.name) ? errores.name : '';
-
+        // errores
+        if (arrayerroresNombre.length > 0) {
+            e.preventDefault();
+            erroresNombre.innerHTML = ''; // Clear any previous error messages
+            for (let i = 0; i < arrayerroresNombre.length; i++) {
+                erroresNombre.innerHTML += "<p>" + arrayerroresNombre[i] + "</p>";
+            }
         } else {
-            erNombre.innerHTML = ''; // Clear the error message if there are no errors
-            registerForm.submit();
+            erroresNombre.innerHTML = ''; // Clear the error message if there are no errors
         }
-    })
-})
+
+        // MAIL
+        if (campomail.value.trim() === "") {
+            errores.push("El campo de correo electrónico debe estar completo");
+        } else {
+            const emailPattern = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+            if (!emailPattern.test(campomail.value.trim())) {
+                errores.push("El correo electrónico ingresado no es válido");
+            }
+        }
+        // errores
+        if (arrayerroresMail.length > 0) {
+            e.preventDefault();
+            erroresMail.innerHTML = ''; // Clear any previous error messages
+            for (let i = 0; i < arrayerroresMail.length; i++) {
+                erroresMail.innerHTML += "<p>" + arrayerroresMail[i] + "</p>";
+            }
+        } else {
+            erroresMail.innerHTML = ''; // Clear the error message if there are no errors
+        }
+
+        // PASSWORD
+        if (campoPassword.value === "") {
+            errores.push("El campo de contraseña debe estar completo");
+        } else {
+            let regex = /^(?=.\d)(?=.[a-záéíóúüñ]).*[A-ZÁÉÍÓÚÜÑ]/;
+            if (!regex.test(campoPassword.value)) {
+            errores.push("Tu contraseña debe tener por lo menos un dígito, una letra minúscula y una letra mayúscula");
+            }
+        }
+        // errores
+        if (arrayerroresPassword.length > 0) {
+            e.preventDefault();
+            erroresPassword.innerHTML = ''; // Clear any previous error messages
+            for (let i = 0; i < arrayerroresPassword.length; i++) {
+                erroresPassword.innerHTML += "<p>" + arrayerroresPassword[i] + "</p>";
+            }
+        } else {
+            erroresPassword.innerHTML = ''; // Clear the error message if there are no errors
+        }
+
+    });
+});
+
